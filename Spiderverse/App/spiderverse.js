b1bdc35849dcebc0fb94301002eadc3c56e79e3a
Daniel Villarreal <DV399697@gmail.com>
2022-05-07 15:32:22 -0500

    Segunda prueba completada

---
+++ Spiderverse/App/spiderverse.js
@@ -0,0 +1,10 @@
+class Spiderman {
+    constructor (name , actor, edad , peliculas, estudio){
+        this.name = name
+        this.actor = actor
+        this.edad = edad
+        this.peliculas =  peliculas
+        this.estudio = estudio
+    }
+}
+module.exports = Spiderman
--- Spiderverse/Test/Spiderverse.test.js
+++ Spiderverse/Test/Spiderverse.test.js
@@ -1,6 +1,19 @@
+const Spiderman = require(`./../App/spiderverse`)
 describe("Test Suite Dummy Description", () => {
     test('Case 1 Dummy', () => {
       const resultOfSomething = 1 + 2
       expect(resultOfSomething).toBe(3);
     });
+  })
+
+  describe("Prueba para clase ´spiderman´", () => {
+      test(`Caso 1`, () => {
+          const AndrewGarfield = new Spiderman("Peter Parker", "Andrew Garfield", 31, 3, "Sony")
+      
+      expect(AndrewGarfield.name).toBe("Peter Parker")
+      expect(AndrewGarfield.actor).toBe("Andrew Garfield")
+      expect(AndrewGarfield.edad).toBe(31)
+      expect(AndrewGarfield.peliculas).toBe(3)
+      expect(AndrewGarfield.estudio).toBe("Sony")
+    })
   })
